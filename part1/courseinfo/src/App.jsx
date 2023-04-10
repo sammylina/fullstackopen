6314f0fb311a367051f8e4520a4f9fedd32aa01f
sammyALX <samuelhailu49@gmail.com>
2023-04-10 09:47:12 +0300

    courseinfo 1.4 completed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,25 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [
+    {
     course: 'Fundamentals of React',
     exercises : 10
-  }
-  const part2 = {
+  },
+   {
     course: 'Using props to pass data',
     exercises : 7
-  }
-  const part3 = {
+  },
+   {
     course: 'State of a component',
     exercises : 14
-  }
+  }]
 
-  const sum = part1.exercises + part2.exercises + part3.exercises;
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={[part1, part2, part3]}/>
-      <Total sum={sum}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
@@ -28,9 +28,9 @@ const Header = ({course}) => (
       <h1>{course}</h1>
 )
 
-const Content = ({parts: [first, part2, part3]}) => {
+const Content = ({parts: [part1, part2, part3]}) => {
     return (<>
-      <Part part={first}/>
+      <Part part={part1}/>
       <Part part={part2}/>
       <Part part={part3}/>
     </>
@@ -42,7 +42,8 @@ const Part = ({part}) => {
         {part.course} {part.exercises}
       </p>)
 }
-const Total = ({sum}) => (
-      <p>Number of exercises {sum}</p>
-)
+const Total = ({parts: [part1, part2, part3]}) => {
+      const sum = part1.exercises + part2.exercises + part3.exercises;
+      return <p>Number of exercises {sum}</p>
+}
 export default App
