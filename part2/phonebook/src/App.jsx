2ee37345048811d987a5544fdcbb88a5f29b9f2f
SammyAlx <samuelhailu49@gmail.com>
2023-05-21 15:18:10 +0300

    phonebook 2.6 completed

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const onNameChange = (e) => {
+    setNewName(e.target.value); 
+  }
+
+  const addPerson = (e) => { 
+    e.preventDefault(); 
+    const newPerson = {name: newName};
+    setPersons([...persons, newPerson])
+    setNewName('');
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={onNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+  
+      {
+        persons.map(person => (<p key={person.name}>{person.name}</p>))
+      }
+
+    </div>
+  )
+
+}
+
+export default App
